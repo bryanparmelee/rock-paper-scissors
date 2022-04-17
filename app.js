49d9a94f0de66520533498ae6ffe9c73880cb4b2
bryanparmelee <bryan.parmelee@gmail.com>
2022-04-17 19:28:54 -0400

    Create playRound function
    Made playRound function that accepts the player's choice and computer's choice as arguments.

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const computerPlay = () => {
-    let compChoice = ['Rock', 'Paper', 'Scissors'];
+    let compChoice = ['ROCK', 'PAPER', 'SCISSORS'];
     let random = Math.floor(Math.random() * compChoice.length);
     console.log(compChoice[random]);
 }
 
-computerPlay();
+
+
+const playRound = (playerSelection, computerSelection) => {
+    const regex = /rock|paper|scissors/i;
+    let response = prompt("Rock, Paper or Scissors?");
+    if (!regex.test(response)) {
+        alert("I did not recognize that response. PLease try again.");
+    } else {
+        playerSelection = response.toUpperCase();
+    }
+
+    computerSelection = computerPlay();
+
+    if (playerSelection === computerSelection) {
+            return "Tie! Choose again.";
+    } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+        return "You lose! Paper beats Rock";
+    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        return "You win! Rock beats Scissors!";
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        return "You lose! Scissors beats Paper!";
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+        return "You win! Paper beats Rock!";
+    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        return "You lose! Rock beats Scissors!";
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        return "You win! Scissors beats Paper";
+    }
+}
+
+playRound();
