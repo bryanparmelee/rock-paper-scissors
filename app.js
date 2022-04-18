eef014e0296b8c308475807295a6d6cfb2e32b83
bryanparmelee <bryan.parmelee@gmail.com>
2022-04-18 14:41:26 -0400

    Update computerPlay Function
    This function was returning an array index instead of the corresponding
    item. Updated to return ROCK, PAPER or SCISSORS at random.

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const computerPlay = () => {
     let compChoice = ['ROCK', 'PAPER', 'SCISSORS'];
     let random = Math.floor(Math.random() * compChoice.length);
-    console.log(compChoice[random]);
+    return compChoice[random];
 }
 
+console.log(computerPlay());
 
-const userChoice = () => {
-    const regex = /rock|paper|scissors/i;
-    let response = prompt("Rock, Paper or Scissors?");
-    if (!regex.test(response)) {
-        alert("I did not recognize that response. Please try again.");
-        userChoice();
-        } else {
-        return response.toUpperCase();
-        }    
-    } 
+// const userChoice = () => {
+//     const regex = /rock|paper|scissors/i;
+//     let response = prompt("Rock, Paper or Scissors?");
+//     if (!regex.test(response)) {
+//         alert("I did not recognize that response. Please try again.");
+//         userChoice();
+//         } else {
+//         return response.toUpperCase();
+//         }    
+//     } 
 
 
 
@@ -22,22 +23,29 @@ const userChoice = () => {
 
 const playRound = (playerSelection, computerSelection) => {
      if (playerSelection === computerSelection) {
-            return "Tie! Choose again.";
+            alert("Tie! Choose again.");
     } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-        return "You lose! Paper beats Rock";
+        alert("You lose! Paper beats Rock");
     } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
-        return "You win! Rock beats Scissors!";
+        alert("You win! Rock beats Scissors!");
     } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-        return "You lose! Scissors beats Paper!";
+        alert("You lose! Scissors beats Paper!");
     } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-        return "You win! Paper beats Rock!";
+        alert("You win! Paper beats Rock!");
     } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-        return "You lose! Rock beats Scissors!";
+        alert("You lose! Rock beats Scissors!");
     } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-        return "You win! Scissors beats Paper";
+        alert("You win! Scissors beats Paper");
     }
  
 }
-computerSelection = computerPlay();
-playerSelection = userChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+
+
+// const game = () => {
+//     for (let i = 0; i < 5; i++) {
+//         let computerSelection = computerPlay();
+//         let playerSelection = userChoice();
+//         console.log(playRound(playerSelection, computerSelection));
+//     }
+// }
