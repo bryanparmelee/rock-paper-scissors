7164ac541bef15680062ee58c6a975659f40750d
bryanparmelee <bryan.parmelee@gmail.com>
2022-04-18 16:20:03 -0400

    Tweak userChoice Regex
    The userChoice Regex was allowing users to play phrases like
    "ROCKER" which wouldn't work with the playRound logic. Updated this
    regex to only allow rock paper and scissors with no additional characters
    or spaces.

--- app.js
+++ app.js
@@ -4,41 +4,41 @@ const computerPlay = () => {
     return compChoice[random];
 }
 
-console.log(computerPlay());
-
-// const userChoice = () => {
-//     const regex = /rock|paper|scissors/i;
-//     let response = prompt("Rock, Paper or Scissors?");
-//     if (!regex.test(response)) {
-//         alert("I did not recognize that response. Please try again.");
-//         userChoice();
-//         } else {
-//         return response.toUpperCase();
-//         }    
-//     } 
 
 
+const userChoice = () => {
+    const regex = /^rock(?!\d|\w|\s)|^paper(?!\d|\w|\s)|^scissors(?!\d|\w|\s)/i;
+    let response = prompt("Rock, Paper or Scissors?");
+    if (!regex.test(response)) {
+        alert("I did not recognize that response. Please try again.");
+        userChoice();
+        } else {
+        return response.toUpperCase();
+        }    
+    } 
 
 
 
-const playRound = (playerSelection, computerSelection) => {
-     if (playerSelection === computerSelection) {
-            alert("Tie! Choose again.");
-    } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
-        alert("You lose! Paper beats Rock");
-    } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
-        alert("You win! Rock beats Scissors!");
-    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-        alert("You lose! Scissors beats Paper!");
-    } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
-        alert("You win! Paper beats Rock!");
-    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
-        alert("You lose! Rock beats Scissors!");
-    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
-        alert("You win! Scissors beats Paper");
-    }
+
+
+// const playRound = (playerSelection, computerSelection) => {
+//      if (playerSelection === computerSelection) {
+//             alert("Tie! Choose again.");
+//     } else if (playerSelection === "ROCK" && computerSelection === "PAPER") {
+//         alert("You lose! Paper beats Rock");
+//     } else if (playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+//         alert("You win! Rock beats Scissors!");
+//     } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+//         alert("You lose! Scissors beats Paper!");
+//     } else if (playerSelection === "PAPER" && computerSelection === "ROCK") {
+//         alert("You win! Paper beats Rock!");
+//     } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+//         alert("You lose! Rock beats Scissors!");
+//     } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+//         alert("You win! Scissors beats Paper");
+//     }
  
-}
+// }
 
 
 
