7c5f76a732a2f229811c8c39dfa60f44cf6c8905
bryanparmelee <bryan.parmelee@gmail.com>
2022-04-21 15:47:46 -0400

    Add running score
    Edited the playRound function to increase the user and computer's score after each round. After 5 a winner is announced, and the scores are reset to 0.

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const scissors = document.querySelector('#scissors');
 const uChoice = document.querySelector('#user-choice');
 const cChoice = document.querySelector('#comp-choice');
 const theResult = document.querySelector('.result');
+const youScore = document.querySelector('#you-score');
+const compScore = document.querySelector('#comp-score');
+let uScore = 0;
+let cScore = 0;
 
 const setPic = (choice) => {
     switch(choice) {
@@ -31,20 +35,12 @@ button.forEach((button) => {
 
 const computerPlay = () => {
     let compChoice = ['rock', 'paper', 'scissors'];
-    let random = Math.floor(Math.random() * compChoice.length);
+    let random = Math.floor(Math.random() * compChoice.length);   
+    cChoice.setAttribute('src', setPic(compChoice[random]));
     return compChoice[random];
 }
 
-// const userChoice = () => {
-//     const regex = /^rock(?!\d|\w|\s)|^paper(?!\d|\w|\s)|^scissors(?!\d|\w|\s)/i;
-//     let response = prompt("Rock, Paper or Scissors?");
-//     if (!regex.test(response)) {
-//         alert("I did not recognize that response. Please try again.");
-//         userChoice();
-//         } else {
-//         return response.toUpperCase();
-//         }    
-//     } 
+
 
 const playRound = (playerSelection, computerSelection) => {
      let result = '';
@@ -52,25 +48,51 @@ const playRound = (playerSelection, computerSelection) => {
             result = "Tie! Choose again." 
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         result = "You lose! Paper beats Rock";
+        cScore++;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         result = "You win! Rock beats Scissors!";
+        uScore++;
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         result = "You lose! Scissors beats Paper!";
+        cScore++;
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         result = "You win! Paper beats Rock!";
+        uScore++
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         result = "You lose! Rock beats Scissors!";
+        cScore++
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         result = "You win! Scissors beats Paper";
+        uScore++;
     }
-        theResult.textContent = result;
+    theResult.textContent = result;
+    youScore.textContent = uScore;
+    compScore.textContent = cScore;
+    if (uScore === 5) {
+        alert("You win! Congrats!");
+        uScore = 0;
+        cScore = 0;
+    } else if (cScore === 5) {
+        alert("You lose! Try again!");
+        uScore = 0;
+        cScore = 0;
+    }
 }
 
-const game = () => {
-    for (let i = 0; i < 5; i++) {
-        const computerSelection = computerPlay();
-        const playerSelection = userChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
+// const game = () => {
+   
+//    if (uScore === 5) {
+//        alert("You win! Congrats!");
+//        uScore = 0;
+//        cScore = 0;
+//        game();
+//    } else if (cScore === 5) {
+//        alert("You lose! Try again!");
+//        uScore = 0;
+//        cScore = 0;
+//        game();
+//    }
+// }
+
+// game();
 
